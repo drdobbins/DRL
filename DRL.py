2f8a6ee70d39507866c2ba7c6cfa8ff3ee71a893
drdobbins <31011287+drdobbins@users.noreply.github.com>
2018-10-22 13:25:04 -0500

    updated version change history

--- DRL.py
+++ DRL.py
@@ -5,6 +5,9 @@
 #Includes slow clock bug, and 3 dots of death fix.
 #2.0.3-Resizing of logos for different resolutions
 #2.0.4-IPF 3rd timer requirement.
+#2.0.4- World Games RS232 Compatability
+#2.0.5- LiftingCast Integration
+#2.0.6-Auto Update functionality
 
 #Created by Scott Dobbins
 #This software shall not be redistrubted without the expressed written consent of Scott Dobbins
